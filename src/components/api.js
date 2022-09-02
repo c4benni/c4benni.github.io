d74ebc6287e1d605050bc8f8a596990f19e13c9b
Chidi Benedict <c4benni@gmail.com>
2022-09-02 19:35:31 +0100

    feat: redeploy

--- src/components/api.js
+++ src/components/api.js
@@ -1,18 +1,19 @@
 import { createClient } from "@supabase/supabase-js";
 
+const SUPABASE_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNDY3MDY4OSwiZXhwIjoxOTUwMjQ2Njg5fQ.q54IGtIcPovjtDAIAcfj70f_jel93V4_47gm-A0s4D0";
 
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzE5Mjg4NCwiZXhwIjoxOTUyNzY4ODg0fQ.ccQeuD2ticHOpbWQ64Z5Re9SESSfxWCv_PJU472PUfU'
-
-const SUPABASE_URL = "https://etllflmdfvutclujgeah.supabase.co"
+const SUPABASE_URL = "https://itwmctclrhnoeqnsakvq.supabase.co";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
-export const postMessage =async ({
-    name,email,message
-}) => {
-    const { data, error } = await supabase.from('message').insert({ name, email, message })
-    
-    return {
-        data,error
-    }
-}
+export const postMessage = async ({ name, email, message }) => {
+  const { data, error } = await supabase
+    .from("message")
+    .insert({ name, email, message });
+
+  return {
+    data,
+    error,
+  };
+};
